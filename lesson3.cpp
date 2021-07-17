08d0530e403e999372094468ec7717c42d1bc1f3
hkl-iii <rahleggon@gmail.com>
2021-07-17 13:01:39 -0500

    updated all code to fit listing 3.3 in book for global vars.

--- lesson3.cpp
+++ lesson3.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 using namespace std;
 
-// three global integers
+// three global integers 
 int firstNumber = 0;
 int secondNumber = 0;
 int multiplicationResult = 0;
 
 void MultiplyNumbers ()
 {
-    cout << "Enter the first number. ";
+    cout << "Enter the first number: ";
     cin >> firstNumber;
 
-    cout << "Enter the second number. ";
+    cout << "Enter the second number: "
     cin >> secondNumber;
 
-    // Multiply two numbers, stire result in a variable 
-    int multiplicationResult = firstNumber * secondNumber;
+    // Multiply two numbers, store result in a variable 
+    multiplicationResult = firstNumber * secondNumber;
 
     // Display result
+    cout << "Displaying from MultiplyNumbers(): ";
     cout << firstNumber << " x " << secondNumber;
-    cout << " = " << multiplicationResult << endl;
+    cout << " = " <<multiplicationResult << endl;
 }
-int main()
+int main ()
 {
-    cout << "This program will help you multiply two numbers." << endl;
+    cout << "This program will help you multiply two numbers" << endl;
 
     // Call the function that does all the work
-    MultiplyNumbers();
+    MultiplyNumbers ();
 
-    cout << firstNumber << " x " << secondNumber;
+    cout << "Displaying from main(): ";
+
+    // This line will now compile and work!
+    cout << first << " x " << secondNumber;
     cout << " = " << multiplicationResult << endl;
 
     return 0;
