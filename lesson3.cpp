02bdf83171b5c54779363f4cd806ecdfdd7e71be
hkl-iii <rahleggon@gmail.com>
2021-07-16 21:00:44 -0500

    added code early

--- lesson3.cpp
+++ lesson3.cpp
@@ -1,4 +1,24 @@
-sildfhasñkfndskl
-sdfasdfdsdf
-sdfadsfasfds
-asdfdsaf
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This program will help you multiply two numbers" << endl;
+
+    cout << "Enter the first number.";
+    int firstNumber = 0;
+    cin >> firstNumber;
+
+    cout << "Enter the second number.";
+    int secondNumber = 0;
+    cin >> secondNumber;
+
+    //Multiply two numbers, stire result in a variable 
+    int multiplicationResult = firstNumber * secondNumber;
+
+    //Display result
+    cout << firstNumber << " x " << secondNumber;
+    cout << " = " << multiplicationResult << endl;
+
+    return 0;
+}
