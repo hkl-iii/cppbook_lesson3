4c808e486823faa0fa8a8ce2b9d386a9e64e3c8a
hkl-iii <rahleggon@gmail.com>
2021-07-17 21:12:21 -0500

    added susints file and sizeof file with code

---
+++ susints.cpp
@@ -0,0 +1,16 @@
+// Signed and Unsigned Integers Examples 
+
+// Signed Int Types = short, int, long, longlong
+
+    short int gradesInMath = -5; // not your best score
+    int moneyInBank = -70000; // overdraft
+    long populationChange = -85000; // reducing population
+    long long countryGDPChange = -70000000000;
+
+/* Unsigned Int Types = unsigned short, unsigned int, 
+   unsigned long, unsigned long long */
+
+   unsigned short int numColorsInRainbow = 7;
+   unsigned int numEggsInBasket = 24; // will always be positive
+   unsigned long numCarsInNewYork = 700000;
+   unsigned long long countryMedicareExpense = 70000000000; 
