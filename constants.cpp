47b79ae18fb43389245896f0b6f2567e84ec1fee
hkl-iii <rahleggon@gmail.com>
2021-07-18 17:21:47 -0500

    changed code and built file of constantcpp to constexpr assignment lesson3 of book

--- constants.cpp
+++ constants.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+constexpr double GetPi() { return 22.0 / 7; }
+constexpr double TwicePi() { return 2 * GetPi(); }
 
 int main()
 {
     using namespace std;
-
     const double pi = 22.0 / 7;
-    cout << "The value of constant pi is: " << pi << endl;
 
-    // Uncomment next line to view compile failure
-    // pi = 345;
-
+    cout << "constant pi contains value " << pi << endl;
+    cout << "constexpr GetPi() returns value " << GetPi() << endl;
+    cout << "constexpr TwicePi() returns value " << TwicePi() << endl;
+    
     return 0;
 }
