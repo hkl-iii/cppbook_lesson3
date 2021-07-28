bb99a57707aa210ae9a6398a23fdb1abf625f95b
hkl-iii <rahleggon@gmail.com>
2021-07-27 20:49:03 -0500

    added comment line 13

--- quiz.cpp
+++ quiz.cpp
@@ -10,7 +10,7 @@ void FindArea ()
     cout << "Please enter the radius: ";
     cin >> radius;
     cout << "The value you entered is: " << radius << endl;
-    int area = pi * radius * radius;
+    int area = pi * radius * radius; //variable must be declared here in order to convert float to an int
     cout << "So the area of your circle is " << area << endl;
 }
 
