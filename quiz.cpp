d44af4339449cc673f0eec395e85cf2d51a2de11
hkl-iii <rahleggon@gmail.com>
2021-07-27 20:08:07 -0500

    answered question for quiz number 3 in quizcpp

---
+++ quiz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{ 
+unsigned short int gohan  = 33;
+signed int krillin = 33;
+long int vegeta = 9000;
+
+cout << "Baby Gohan's power level is " << gohan << endl;
+cout << "Krillin's power level is " << krillin << endl;
+cout << "But Vegeta's power level is over " <<vegeta << "!!!!" << endl  << endl;
+
+
+
+
+return 0;
+}
